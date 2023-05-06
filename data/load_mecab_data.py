60250838c2287efaec6f401bc1601937d227f3e9
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-05-06 16:06:37 +0900

    Update load_mecab_data.py

--- data/load_mecab_data.py
+++ data/load_mecab_data.py
@@ -25,7 +25,7 @@ def load_mecab_dataset(test_ratio=0.2, erase_comma=True, random_state=None):
         if erase_comma:
             texts = [(row[1] if row[1][:2] != "광고" else row[1][2:]).replace(",", "") for row in reader]
         else:
-            texts = [row[1].replace("광고", "", 1) for row in reader]
+            texts = [row[1] if row[1][:2] != "광고" else row[1][2:] for row in reader]
     with open('data/mecab_train_y.csv', 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
         labels = torch.FloatTensor(np.array([int(row[1]) for row in reader]).reshape(-1, 1)).detach()
