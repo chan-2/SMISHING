5b725eaf0365763fce2e3341101a473522449704
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-05-06 16:01:56 +0900

    Erase "광고" string at the front of texts if it exists
    Erase "광고" string at the front of texts if it exists

--- data/load_mecab_data.py
+++ data/load_mecab_data.py
@@ -23,9 +23,9 @@ def load_mecab_dataset(test_ratio=0.2, erase_comma=True, random_state=None):
     with open('data/mecab_train_x.csv', 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
         if erase_comma:
-            texts = [row[1].replace(",", "") for row in reader]
+            texts = [(row[1] if row[1][:2] != "광고" else row[1][2:]).replace(",", "") for row in reader]
         else:
-            texts = [row[1] for row in reader]
+            texts = [row[1].replace("광고", "", 1) for row in reader]
     with open('data/mecab_train_y.csv', 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
         labels = torch.FloatTensor(np.array([int(row[1]) for row in reader]).reshape(-1, 1)).detach()
