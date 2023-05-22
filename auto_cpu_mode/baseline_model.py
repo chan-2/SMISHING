2a958e0d1a3e16179b7955cf89c53748b8626e2e
chan-2 <44949723+chan-2@users.noreply.github.com>
2023-05-22 14:39:38 +0900

    Add files via upload

---
+++ auto_cpu_mode/baseline_model.py
@@ -0,0 +1,35 @@
+import torch
+from torch import nn
+from transformers import AutoTokenizer
+
+
+class BaselineModel(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super().__init__()
+        self.device = "cpu"
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.checkpoint = "beomi/KcELECTRA-base-v2022"
+        self.tokenizer = AutoTokenizer.from_pretrained(self.checkpoint)
+        self.embedding = nn.Embedding(num_embeddings=54329, embedding_dim=input_size)
+        self.dropout = 0
+        self.num_layers = 1
+        self.rnn = nn.RNN(input_size=input_size, hidden_size=hidden_size, num_layers=self.num_layers,
+                          nonlinearity="tanh", batch_first=True)
+        self.linear = nn.Sequential(nn.Linear(hidden_size, 10),nn.Linear(10, 1))
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, X: torch.Tensor, return_embedding=False):
+        embeddings = self.embedding(X)
+        N = X.shape[0]
+        h_0 = torch.zeros((self.num_layers, N, self.hidden_size))
+        y, _ = self.rnn(embeddings, h_0)
+        output = self.sigmoid(self.linear(y[:, -1, :]))
+        if return_embedding:
+            return output, y[:, -1, :]
+        else:
+            return output
+
+    def tokenize_texts(self, texts):
+        tokenized_texts = self.tokenizer(texts, padding=True, return_tensors="pt")
+        return tokenized_texts['input_ids']
